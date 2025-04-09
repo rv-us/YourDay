5063f3a5a531c6f625f5464fb6d90e450286a7b6
rv-us <140857704+rv-us@users.noreply.github.com>
2025-04-08 20:07:30 -0700

    Initial Commit

---
+++ YourDay/YourDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourDayApp.swift
+//  YourDay
+//
+//  Created by Rachit Verma on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct YourDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
